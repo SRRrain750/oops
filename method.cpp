a7dfa954311a49331e21484d424e51ed394dd148
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-04-16 22:27:43 +0530

    Add files via upload

---
+++ method.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class manth{
+string name;
+string day;
+string date;
+
+
+public:
+setter(string NAME){
+
+name=NAME;
+
+}
+getter(){
+
+cout<<"Name:"<<name;
+
+};
+
+
+
+int main(){
+
+manth obj1;
+obj1.setter("august");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
